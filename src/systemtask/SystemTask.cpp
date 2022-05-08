2feb17c3e8d7eceba7f15707bf3b0c5b6d594615
Finlay Davidson <finlay.davidson@coderclass.nl>
2022-05-08 16:52:39 +0200

    Update to new SystemTask states

--- src/systemtask/SystemTask.cpp
+++ src/systemtask/SystemTask.cpp
@@ -503,7 +503,7 @@ void SystemTask::UpdateMotion() {
       motionController.Should_ShakeWake(settingsController.GetShakeThreshold())) {
     GoToRunning();
   }
-  if (settingsController.isWakeUpModeOn(Pinetime::Controllers::Settings::WakeUpMode::LowerWrist) && !isSleeping &&
+  if (settingsController.isWakeUpModeOn(Pinetime::Controllers::Settings::WakeUpMode::LowerWrist) && state == SystemTaskState::Running &&
       motionController.ShouldLowerSleep()) {
     PushMessage(Messages::GoToSleep);
   }
